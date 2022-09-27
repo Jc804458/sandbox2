483e58e1e469adac4c1d3a48781673a781222b66
jc804458 <akachukwu.madujibeya@my.jcu.edu.edu>
2022-09-27 17:00:21 +1000

    update answers on random.py file

--- prac3/randoms.py
+++ prac3/randoms.py
@@ -5,9 +5,9 @@ print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
 # What did you see on line 1?
-# line 1 was returning a random number between the range of 5 and 19.
+# line 1 was returning a random number between the range of 5 and 20.
 # What was the smallest number you could have seen, what was the largest?
-# Ans: The smallest number could have been 5 and the largest could have been 19
+# Ans: The smallest number could have been 5 and the largest could have been 20
 
 
 # What did you see on line 2?
@@ -21,9 +21,9 @@ print(random.uniform(2.5, 5.5))  # line 3
 # What did you see on line 3?
 # Ans: line 3 was returning random floating number between 2.5 and 5.5
 # What was the smallest number you could have seen, what was the largest?
-# Ans: The smallest number could have been 2.5 while the largest number could have been 5.49999
+# Ans: The smallest number could have been 2.5 while the largest number could have been 5.5
 
 
 # the net line of code is to be run on the python console
-import random
+
 print(random.randint(1, 100))
