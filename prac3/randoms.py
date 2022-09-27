77d4a1a5c37c51ec3d51a7f69b8cc5adffb91e6c
jc804458 <akachukwu.madujibeya@my.jcu.edu.edu>
2022-09-27 16:46:16 +1000

    complete  questions on ramdon file

---
+++ prac3/randoms.py
@@ -0,0 +1,29 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# line 1 was returning a random number between the range of 5 and 19.
+# What was the smallest number you could have seen, what was the largest?
+# Ans: The smallest number could have been 5 and the largest could have been 19
+
+
+# What did you see on line 2?
+# Ans: line 2 was returning random values of 3, 5, 7 and 9.
+# What was the smallest number you could have seen, what was the largest?
+# Ans: 3 was the smallest number and 9 was the largest number i could have seen
+# Could line 2 have produced a 4?
+# Ans: No, line2 could not have produced 4.
+
+
+# What did you see on line 3?
+# Ans: line 3 was returning random floating number between 2.5 and 5.5
+# What was the smallest number you could have seen, what was the largest?
+# Ans: The smallest number could have been 2.5 while the largest number could have been 5.49999
+
+
+# the net line of code is to be run on the python console
+import random
+print(random.randint(1, 100))
